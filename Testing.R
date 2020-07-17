d6bf7d9f215ab8a15f3b6af2586a12b84e4e075d
SLAZER <satyamm736@gmail.com>
2020-07-17 11:07:17 +0530

    Initial commit to Testing.R My first Commit to GitHub.

---
+++ Testing.R
@@ -0,0 +1,4 @@
+print("This file was created on R Studio")
+print("My first actual Repository with GitHub and a step to learn R")
+print("This file was created on June 2020")
+print("And Now it lives on Github")
